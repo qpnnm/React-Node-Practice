8998c659c1c287e3c863f02b1ee39f31269cc497
Bik Cha <https://github.com/qpnnm/publish.git>
2021-08-08 03:03:58 +0900

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
-function App() {
-  return (
-    <div className="gray-background">
-  
-    <img src={logo} lat= "logo"/>
-    <h2>Let's Develop React</h2>
 
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961212',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '양의지',
+  'birthday': '982154',
+  'gender': '여자',
+  'job': '고딩'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '김삼영',
+  'birthday': '950121',
+  'gender': '남자',
+  'job': '쎈캐'
+}
+]
+class App extends Component {
+  render() {
+    return (
+      <div>
 
-    </div>
-  );
+        {customers.map(c => {
+
+          return (<Customer
+            key = {c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday = {c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />)
+
+        })}
+
+      </div>
+    )
+
+  }
 }
 
 export default App;
